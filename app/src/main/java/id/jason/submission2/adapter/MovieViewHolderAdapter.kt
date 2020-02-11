6a71e8884ca03e91ccd73a3be1cbea229ac4385f
Jason Nathanael Tjahjadi <jason.tjahjadi@otomoto.id>
2020-02-11 14:35:18 +0700

    change itemview context in adapter

--- app/src/main/java/id/jason/submission2/adapter/MovieViewHolderAdapter.kt
+++ app/src/main/java/id/jason/submission2/adapter/MovieViewHolderAdapter.kt
@@ -9,45 +9,47 @@ import com.bumptech.glide.Glide
 import id.jason.submission2.R
 import id.jason.submission2.model.Shows
 import id.jason.submission2.view.ShowDetailActivity
-import kotlinx.android.synthetic.main.item_list.view.*
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list.*
 
 class MovieViewHolderAdapter(private val listShows: ArrayList<Shows>) :
-    RecyclerView.Adapter<MovieViewHolderAdapter.MoviewViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviewViewHolder {
+    RecyclerView.Adapter<MovieViewHolderAdapter.MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
-        return MoviewViewHolder(view)
+        return MovieViewHolder(view)
     }
 
     override fun getItemCount(): Int = listShows.size
 
 
-    override fun onBindViewHolder(holder: MoviewViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
         holder.setView(listShows[position])
     }
 
-    inner class MoviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun setView(shows: Shows) {
-            with(itemView) {
-                Glide.with(itemView.context)
-                    .load(shows.showPhoto)
-                    .into(list_image)
-                list_title.text = shows.showTitle
-                list_date.text = shows.showDate
-                list_rating.text = shows.showRating
-                list_summary.text = shows.showDesc
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), LayoutContainer {
+        override val containerView: View?
+            get() = itemView
 
-                itemView.setOnClickListener {
-                    val movieSend = Shows(
-                        shows.showTitle,
-                        shows.showDate,
-                        shows.showDesc,
-                        shows.showRating,
-                        shows.showPhoto
-                    )
-                    val intentToDetail = Intent(itemView.context, ShowDetailActivity::class.java)
-                    intentToDetail.putExtra(ShowDetailActivity.EXTRA_SHOWS, movieSend)
-                    itemView.context.startActivity(intentToDetail)
-                }
+        fun setView(shows: Shows) {
+            Glide.with(itemView.context)
+                .load(shows.showPhoto)
+                .into(list_image)
+            list_title.text = shows.showTitle
+            list_date.text = shows.showDate
+            list_rating.text = shows.showRating
+            list_summary.text = shows.showDesc
+
+            itemView.setOnClickListener {
+                val movieSend = Shows(
+                    shows.showTitle,
+                    shows.showDate,
+                    shows.showDesc,
+                    shows.showRating,
+                    shows.showPhoto
+                )
+                val intentToDetail = Intent(itemView.context, ShowDetailActivity::class.java)
+                intentToDetail.putExtra(ShowDetailActivity.EXTRA_SHOWS, movieSend)
+                itemView.context.startActivity(intentToDetail)
             }
         }
     }
